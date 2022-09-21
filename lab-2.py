def836a5258bbdd0442fe006441dba80cb2fafdc
zaidalshreef <zaidalshreef@gmail.com>
2022-09-21 12:16:08 +0300

    lab-2

---
+++ lab-2.py
@@ -0,0 +1,48 @@
+
+# Question 1 :
+
+
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(list)
+list.append(10)
+print(list)
+list.remove(5)
+print(list)
+list.reverse()
+print(list)
+list.sort()
+print(list)
+list.insert(0, 100)
+print(list)
+print(list.index(list[len(list) - 1]))
+
+
+# Question 2 :
+
+people = ["Ahmed", "Nasser", "Mohammed"]
+
+print(' '.join(people))
+
+
+
+# Question 3 :
+
+dic = [{"name":"zaid" , "phone_number":"05034545545"},
+       {"name":"ahmad" , "phone_number":"050767677"},
+       {"name":"faisal" , "phone_number":"0507777"},]
+
+print(dic)
+
+dic.append({"name":"faris" , "phone_number":"050332999"} )
+
+print("add person to list ",dic)
+
+dic[0].pop('name')
+
+print("delete name from first item :",dic)
+
+dic[-1]["phone_number"] = "050999999999"
+
+print("key is found in last item :","phone_number" in dic[-1].keys())
