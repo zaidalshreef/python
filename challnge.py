bf6ee4425a89b3acc974a2b18e45a1f6c2ce46b0
zaidalshreef <zaidalshreef@gmail.com>
2022-09-27 11:15:22 +0300

    abs

--- challnge.py
+++ challnge.py
@@ -1,6 +1,6 @@
 # if the list contain negative numbers, the function will not return list end with zeros
 def sort_zeros(list):
-    return sorted(list, reverse=True)
+    return sorted(list, key=abs, reverse=True)
 
 # if the list contain negative numbers or any latter, the function will return list end with zeros
 def zeros_list(list):
@@ -16,3 +16,8 @@ def zeros_list(list):
 number = [-10, 1, 2, 0, 0, 3, -15, 4, 5, 0, 6, 0, 7, 8, 9, -1]
 print(sort_zeros(number))
 print(zeros_list(number))
+
+
+
+
+
